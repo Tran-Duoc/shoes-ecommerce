459f31af0813315f98b1ba679899fa8e6e8fc70b
Tran Duoc <duoctran2527@gmail.com>
2023-08-31 15:35:37 +0700

    install package, validation value schema

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
@@ -21,5 +22,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
