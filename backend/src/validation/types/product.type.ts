56db19c4c9f4cef33d3239580413643741b18fd5
Tran Duoc <duoctran2527@gmail.com>
2023-09-02 10:17:39 +0700

    reset all work

---
+++ backend/src/validation/types/product.type.ts
@@ -0,0 +1,39 @@
+export enum ShoeSize {
+  EU40,
+  EU41,
+  EU42,
+  EU43,
+  EU44,
+  EU45,
+  EU46,
+  EU47,
+  EU48,
+  EU49,
+  EU50,
+}
+
+export enum Gender {
+  MEN,
+  WOMEN,
+}
+
+export enum Color {
+  WHITE,
+  BLUE,
+  GREEN,
+  ORANGE,
+  RED,
+  BROWN,
+  PINK,
+}
+
+export enum Branch {
+  JORDAN,
+  CONVERSE,
+  NIKE,
+}
+
+export enum Role {
+  USER,
+  ADMIN,
+}
